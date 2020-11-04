8843f8919ccedfcf24473622ef0acbd18aa70d7b
joshuacrask37 <73957551+joshuacrask37@users.noreply.github.com>
2020-11-04 16:24:40 -0600

    Update and rename README.md to README.py

--- README.py
+++ README.py
@@ -3,6 +3,3 @@ just another new repository
 
 Hello.  This is my new line of code.  I'm still trying to determine what languages I can use to code on this program.
 I enjoy learning Github so far, though!
-
-# python
-
