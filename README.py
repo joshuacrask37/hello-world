ed042b08f1345182f6035d63441033d2dc2bacfb
joshuacrask37 <73957551+joshuacrask37@users.noreply.github.com>
2020-11-04 16:25:19 -0600

    Update README.py

--- README.py
+++ README.py
@@ -3,3 +3,5 @@ just another new repository
 
 Hello.  This is my new line of code.  I'm still trying to determine what languages I can use to code on this program.
 I enjoy learning Github so far, though!
+
+print (Hello World)
